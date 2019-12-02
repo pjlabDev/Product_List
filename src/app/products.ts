a6e1a5668b7ed404895c0c3a63d8118e24fc2717
PedrassoxD <pedroperlab@gmail.com>
2019-12-02 19:05:57 +0100

    Nueva revision

---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'Un móvil largo con una de las mejores pantallas.'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'Un buen móvil con una de las mejores cámaras.'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: 'El mejor móvil para cualquier persona.'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
